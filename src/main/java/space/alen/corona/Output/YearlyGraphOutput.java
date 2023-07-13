62d64c929c4866cdc3325d5a44a9091acab940bf
alejandro <acomatico18@gmail.com>
2023-07-13 17:39:06 +0200

    Add the XY axis to the graph
    Gave me some trouble but I think I got it

--- src/main/java/space/alen/corona/Output/YearlyGraphOutput.java
+++ src/main/java/space/alen/corona/Output/YearlyGraphOutput.java
@@ -23,33 +23,44 @@ public class YearlyGraphOutput
             }
         }
 
-        int width = 200;
-        int height = 40;
+        int width = 180;
+        int height = 50;
         int range = maxValue - minValue;
 
-        float heightValue = (float) range / height;
-        float widthValue = (float) (width + 1) / 12;
-        System.out.println(widthValue);
+        float heightRatio = (float) range / height;
+        float widthRatio = (float) width / 12;
 
         Character[][] matrix = new Character[height][width];
 
         for (int i = 0; i < height; i++) {
             for (int j = 0; j < width; j++) {
-                System.out.println(j);
-                int month = (int) Math.floor(j / widthValue);
+                int month = (int) Math.floor(j / widthRatio);
 
                 int cases = yearlyStats.monthlyStats().get(month).cases();
                 
-                if (cases >= i * heightValue) {
-                    matrix[height - i - 1][j] = '▇';
+                if (cases >= i * heightRatio) {
+                    matrix[i][j] = '▇';
                 } else {
-                    matrix[height - i - 1][j] = ' ';
+                    matrix[i][j] = ' ';
                 }
             }
         }
 
-        for (Character[] row : matrix) {
+        int leftSpace = 12;
+
+        for (int i = height - 1; i >= 0; i--) {
+            var row = matrix[i];
+            int casesValue = (int) Math.floor(heightRatio * (i+1));
+
             String line = "";
+            if (i % 5 == 0) {
+                line = "" + casesValue;
+            }
+
+            while (leftSpace > line.length()) {
+                line = " " + line;
+            }
+            line += " | "; 
 
             for (Character cell : row) {
                 line += cell;
@@ -57,5 +68,21 @@ public class YearlyGraphOutput
 
             System.out.println(line);
         }
+
+        String[] monthNames = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
+        String legend = "";
+        while (leftSpace > legend.length()) {
+            legend = " " + legend;
+        }
+        legend += " | "; 
+
+        for (int i = 0; i < 12; i++) {
+            legend += monthNames[i];
+            for (int j = 0; j < 12; j++) {
+                legend += " ";
+            }
+        }
+
+        System.out.println(legend);
     }
 }
